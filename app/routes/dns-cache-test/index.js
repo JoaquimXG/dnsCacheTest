044743bf504e9cbf657b5dd2bd3cb782bf472b11
Joaquim Gomez <49433786+JoaquimXG@users.noreply.github.com>
2022-01-24 18:28:49 +0000

    Functions complete aside from modifying DNS entries

--- app/routes/dns-cache-test/index.js
+++ app/routes/dns-cache-test/index.js
@@ -1,16 +1,32 @@
 const router = require("express").Router()
+const http = require("http")
+const fs = require("fs")
+
+const { PARTNER_ADDRESS } = require("../../utils/dotenvDefault")
 
 WAITING_FOR_CONFIRM = false
+FIRST_REQUEST = true
+
+const dnsLog = dict => {
+    console.log("logging to file")
+    dict = {ts: new Date().toISOString(), ...dict}
+    fs.appendFile("./logs/dns-cache.log", `${JSON.stringify(dict)}\n`,
+     err => { if (err) console.log(err) })
+}
 
 router.get("/test", (req, res) => {
     if (WAITING_FOR_CONFIRM) {
         res.send("Waiting for confirmation from partner server")
     }
     else {
-        //DNS cache has cleared, updating DNS to point to partner server
+        if (!FIRST_REQUEST) {
+            dnsLog({event: "cacheCleared"})
+        }
+
         //TODO point dns to partner server 
-        //TODO log dns cleared time
         WAITING_FOR_CONFIRM = true
+        http.request({ hostname: PARTNER_ADDRESS, port: 80, path: "/dns/confirm", method: "GET" })
+
         res.send("Cache has cleared, updating cache to point to new server")
     }
 })
@@ -18,11 +34,12 @@ router.get("/test", (req, res) => {
 router.get("/confirm", (req, res) => {
     if (WAITING_FOR_CONFIRM) {
         WAITING_FOR_CONFIRM = false
+        FIRST_REQUEST = false
         res.send("Confirmation that DNS has updated to point to second server recieved")
     }
     else {
-        //TODO log this error case
-        res.send(500, "Confirmation recieved before waiting for confirmation")
+        dnsLog({error: true, event: "Confirmation recieved before waiting for confirmation"})
+        res.status(500).send("Confirmation recieved before waiting for confirmation")
     }
 })
 
--- app/utils/dotenvDefault.js
+++ app/utils/dotenvDefault.js
@@ -1,7 +1,9 @@
 require("dotenv").config()
 
 HTTP_PORT = process.env.HTTP_PORT ? process.env.HTTP_PORT : 80
+PARTNER_ADDRESS = process.env.PARTNER_ADDRESS ? process.env.PARTNER_ADDRESS : null
 
 module.exports = {
-    HTTP_PORT
+    HTTP_PORT,
+    PARTNER_ADDRESS
 }
