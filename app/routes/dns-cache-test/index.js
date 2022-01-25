b1fc67da787e8ab31cf1be092bb65809a9f1f6db
Joaquim Gomez <49433786+JoaquimXG@users.noreply.github.com>
2022-01-25 13:55:29 +0000

    Adding additional logging

--- app/routes/dns-cache-test/index.js
+++ app/routes/dns-cache-test/index.js
@@ -1,4 +1,4 @@
-const { PARTNER_DNS, PARTNER_IP, LOCAL_IP } = require("../../utils/dotenvDefault")
+const { PARTNER_DNS, PARTNER_IP, LOCAL_IP, LOCAL_DNS } = require("../../utils/dotenvDefault")
 const router = require("express").Router()
 const http = require("http")
 const axios = require("axios")
@@ -19,13 +19,13 @@ router.get("/test", async (req, res) => {
     else {
         if (!FIRST_REQUEST) {
             dnsCacheDelay = (new Date() - DNS_CHANGE_CONFIRM_TS) /1000
-            dnsLog({ event: "requestRecieved" , server: LOCAL_IP, delay: dnsCacheDelay})
+            dnsLog({ event: "CacheUpdated" , host: LOCAL_DNS, delay: dnsCacheDelay})
         }
         message = `Cache has cleared, pointing DNS to ${PARTNER_IP}`
         log.debug(message)
 
         await updateRoute53()
-        dnsLog({event: "DNSUpdated", target: PARTNER_IP})
+        dnsLog({event: "DNSUpdated", host: LOCAL_DNS, target: PARTNER_IP})
 
         WAITING_FOR_CONFIRM = true
         log.debug(`Sending confirmation to partner: ${PARTNER_DNS}`)
@@ -41,6 +41,7 @@ router.get("/confirm", (req, res) => {
     if (WAITING_FOR_CONFIRM) {
         WAITING_FOR_CONFIRM = false
         DNS_CHANGE_CONFIRM_TS = new Date()
+        dnsLog({event: "ConfirmationReceived", host: LOCAL_DNS})
         message = "Confirmation recieved"
         log.info(message)
         res.send(message)
@@ -48,6 +49,7 @@ router.get("/confirm", (req, res) => {
     else if (FIRST_REQUEST) {
         FIRST_REQUEST = false
         DNS_CHANGE_CONFIRM_TS = new Date()
+        dnsLog({event: "ConfirmationReceived", host: LOCAL_DNS})
         message = "First confirmation received"
         log.info(message)
         res.send(message)
